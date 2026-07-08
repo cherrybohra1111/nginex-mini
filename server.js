cf7c4074b69c79ae5c962b08d300932ca4a794b7
cherrybohra1111 <cherrybohra@gmail.com>
2026-07-08 13:41:58 +0530

    Send plain text HTTP response

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const http = require('http');
 const PORT=3000;
 const HOSTNAME="127.0.0.1";
 
-const server=http.createServer((req) => {
+const server=http.createServer((req, res) => {
     const httpMethod=req.method;
     const url =req.url;
     console.log(`Received a ${httpMethod} request for ${url}`);
+
+    res.writeHead(200, {
+    "Content-Type": "text/plain"
+    });
+    res.end("Hello from Cherry's Server!")
+
+
 });
 
 server.listen(PORT, () => {
