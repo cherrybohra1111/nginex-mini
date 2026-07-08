079b9786dc3d29e8c8c34b06eedc6a86cd197a7c
cherrybohra1111 <cherrybohra@gmail.com>
2026-07-08 12:46:35 +0530

    Handle incoming HTTP requests

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const http = require('http');
 const PORT=3000;
 const HOSTNAME="127.0.0.1";
 
-const server=http.createServer();
+const server=http.createServer((req) => {
+    const httpMethod=req.method;
+    const url =req.url;
+    console.log(`Received a ${httpMethod} request for ${url}`);
+});
 
-server.listen(PORT, HOSTNAME, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
