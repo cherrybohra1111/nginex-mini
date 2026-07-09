b111f4eaa406bed77ccaca6eabcdef290a121472
cherrybohra1111 <cherrybohra@gmail.com>
2026-07-09 18:57:49 +0530

    Add timestamps to custom logger

--- logger.js
+++ logger.js
@@ -1,13 +1,29 @@
+function pad(number){
+    return String(number).padStart(2,"0");
+}
+
+function getTimestamp(){
+    const now = new Date();
+    const year= now.getFullYear();
+    const month = now.getMonth()+1;
+    const date = now.getDate();
+    const hrs = now.getHours();
+    const mins = now.getMinutes();
+    const sec = now.getSeconds();
+
+    return `[${pad(year)}-${pad(month)}-${pad(date)} ${pad(hrs)}:${pad(mins)}:${pad(sec)}]`;
+}
+
 function info(message){
-    console.log(`INFO: ${message}`);
+    console.log(`${getTimestamp()} INFO: ${message}`);
 }
 
 function warn(message){
-    console.log(`WARN: ${message}`);
+    console.log(`${getTimestamp()} WARN: ${message}`);
 }
 
 function error(message){
-    console.log(`ERROR: ${message}`);
+    console.log(`${getTimestamp()} ERROR: ${message}`);
 }
 
 module.exports = {
